ed1bf9d8a83fd0de57ecbabe060eb434a081f1a0
masterteam1 <zozo.prince7@gmail.com>
2016-09-18 21:33:26 +0300

    master_team

--- steady.sh
+++ steady.sh
@@ -42,7 +42,7 @@ NONVOLUNTARYCHECK=0
 VOLUNTARY=1
 VOLUNTARYCHECK=0
 I=1
-BOT=DBTeam  # You can put here other bots. Also you can change it to run more than one bot in the same server.
+BOT=MASTER  # You can put here other bots. Also you can change it to run more than one bot in the same server.
 RELOADTIME=10  # Time between checking cpu calls of the cli process. Set the value high if your bot does not receive lots of messages.
 
 
@@ -69,7 +69,7 @@ cat << EOF
  
 EOF
 echo -e "                \e[100m                Steady script           \e[00;37;40m"
-echo -e "               \e[01;34m                    by iicc                \e[00;37;40m"
+echo -e "               \e[01;34m                    by MASTER                \e[00;37;40m"
 echo ""
 cat << EOF
  $bld$f1▄ ▀▄   ▄▀ ▄   $f2 ▄▄▄████▄▄▄    $f3  ▄██▄     $f4▄ ▀▄   ▄▀ ▄   $f5 ▄▄▄████▄▄▄    $f6  ▄██▄  $rst
@@ -95,11 +95,16 @@ sleep 0.5
 
 
 echo ""
-echo -e "\033[38;5;208m      ____  ____ _____                        \033[0;00m"
-echo -e "\033[38;5;208m     |    \|  _ )_   _|___ ____   __  __      \033[0;00m"
-echo -e "\033[38;5;208m     | |_  )  _ \ | |/ .__|  _ \_|  \/  |     \033[0;00m"
-echo -e "\033[38;5;208m     |____/|____/ |_|\____/\_____|_/\/\_|     \033[0;00m"
-echo -e "\033[38;5;208m                                              \033[0;00m"
+echo -e "\033[38;5;208m"   
+echo -e " ╭━╮╭━╮╱╱╱╱╱╭╮               "
+echo -e " ┃┃╰╯┃┃╱╱╱╱╭╯╰╮              "
+echo -e " ┃╭╮╭╮┣━━┳━┻╮╭╋━━┳━╮           "
+echo -e " ┃┃┃┃┃┃╭╮┃━━┫┃┃┃━┫╭┫           "
+echo -e " ┃┃┃┃┃┃╭╮┣━━┃╰┫┃━┫┃             "
+echo -e " ╰╯╰╯╰┻╯╰┻━━┻━┻━━┻╯             "
+echo -e "                                     \033[0;00m"
+echo -e "\e[36m"                                      
+echo -e "MASTER TEAM" 
 
 sleep 1.5
 echo -e "$bld$f4 CHECKING PROCESSES...$rst"
@@ -227,7 +232,7 @@ cat << EOF
  
 EOF
 echo -e "                \e[100m                Steady script           \e[00;37;40m"
-echo -e "               \e[01;34m                    by iicc                \e[00;37;40m"
+echo -e "               \e[01;34m                    by MASTER                \e[00;37;40m"
 echo ""
 cat << EOF
  $bld$f1▄ ▀▄   ▄▀ ▄   $f2 ▄▄▄████▄▄▄    $f3  ▄██▄     $f4▄ ▀▄   ▄▀ ▄   $f5 ▄▄▄████▄▄▄    $f6  ▄██▄  $rst
@@ -253,11 +258,16 @@ sleep 0.5
 
 
 echo ""
-echo -e "\033[38;5;208m      ____  ____ _____                        \033[0;00m"
-echo -e "\033[38;5;208m     |    \|  _ )_   _|___ ____   __  __      \033[0;00m"
-echo -e "\033[38;5;208m     | |_  )  _ \ | |/ .__|  _ \_|  \/  |     \033[0;00m"
-echo -e "\033[38;5;208m     |____/|____/ |_|\____/\_____|_/\/\_|     \033[0;00m"
-echo -e "\033[38;5;208m                                              \033[0;00m"
+echo -e "\033[38;5;208m"   
+echo -e " ╭━╮╭━╮╱╱╱╱╱╭╮               "
+echo -e " ┃┃╰╯┃┃╱╱╱╱╭╯╰╮              "
+echo -e " ┃╭╮╭╮┣━━┳━┻╮╭╋━━┳━╮           "
+echo -e " ┃┃┃┃┃┃╭╮┃━━┫┃┃┃━┫╭┫           "
+echo -e " ┃┃┃┃┃┃╭╮┣━━┃╰┫┃━┫┃             "
+echo -e " ╰╯╰╯╰┻╯╰┻━━┻━┻━━┻╯             "
+echo -e "                                     \033[0;00m"
+echo -e "\e[36m"                                      
+echo -e "MASTER TEAM" 
 
 # Starting preliminar setup
 sleep 1.5
@@ -539,11 +549,16 @@ while getopts ":tsTSih" opt; do
 	echo "steady.sh bash script v1.2 iicc 2016 DBTeam" >&2
 	echo ""
 	echo -e "\e[0m"
-echo -e "\033[38;5;208m      ____  ____ _____                        \033[0;00m"
-echo -e "\033[38;5;208m     |    \|  _ )_   _|___ ____   __  __      \033[0;00m"
-echo -e "\033[38;5;208m     | |_  )  _ \ | |/ .__|  _ \_|  \/  |     \033[0;00m"
-echo -e "\033[38;5;208m     |____/|____/ |_|\____/\_____|_/\/\_|     \033[0;00m"
-echo -e "\033[38;5;208m                                              \033[0;00m"	
+echo -e "\033[38;5;208m"   
+echo -e " ╭━╮╭━╮╱╱╱╱╱╭╮               "
+echo -e " ┃┃╰╯┃┃╱╱╱╱╭╯╰╮              "
+echo -e " ┃╭╮╭╮┣━━┳━┻╮╭╋━━┳━╮           "
+echo -e " ┃┃┃┃┃┃╭╮┃━━┫┃┃┃━┫╭┫           "
+echo -e " ┃┃┃┃┃┃╭╮┣━━┃╰┫┃━┫┃             "
+echo -e " ╰╯╰╯╰┻╯╰┻━━┻━┻━━┻╯             "
+echo -e "                                     \033[0;00m"
+echo -e "\e[36m"                                      
+echo -e "MASTER TEAM" 
 echo ""
 	exit 1
       ;;
