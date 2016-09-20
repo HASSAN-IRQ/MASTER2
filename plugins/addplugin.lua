b0f50f69ed816a6cddacea5018f03701eb9e98f2
masterteam1 <zozo.prince7@gmail.com>
2016-09-20 16:59:43 +0300

    master_team

--- plugins/addplugin.lua
+++ plugins/addplugin.lua
@@ -1,17 +1,22 @@
--- plugins to add by Mico 
+--" ╭━╮╭━╮╱╱╱╱╱╭╮               "
+--" ┃┃╰╯┃┃╱╱╱╱╭╯╰╮              "      
+--" ┃╭╮╭╮┣━━┳━┻╮╭╋━━┳━╮           "
+--" ┃┃┃┃┃┃╭╮┃━━┫┃┃┃━┫╭┫           "
+--" ┃┃┃┃┃┃╭╮┣━━┃╰┫┃━┫┃             "
+--" ╰╯╰╯╰┻╯╰┻━━┻━┻━━┻╯             "
 do 
 local function run(msg,matches) 
 if is_sudo(msg) and msg.text then 
 local name = matches[1] 
 local file = matches[3] 
-local Ali = get_receiver(msg) 
+local master = get_receiver(msg) 
 
 fi = io.open("./plugins/".. name..".lua","w+") 
 fi:write(file) 
 fi:close() 
-send_large_msg(Ali,"تم👍🏻اضافه الملف📁"..name..".lua",ok_cb,false) 
+send_large_msg(master,"تم👍🏻اضافه الملف📁"..name..".lua",ok_cb,false) 
 end 
--- @lua_lang with @iq_plus 
+
 end 
 return { 
  patterns = { 
@@ -21,3 +26,4 @@ return {
  } 
 
 end 
+--MASTER TEAM -_-
