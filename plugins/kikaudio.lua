3f2ec6ce981685a0f999057ed8b9289f9621b93a
masterteam1 <zozo.prince7@gmail.com>
2016-09-21 15:52:26 +0300

    master_team

--- plugins/kikaudio.lua
+++ plugins/kikaudio.lua
@@ -1,19 +1,14 @@
+--" ╭━╮╭━╮╱╱╱╱╱╭╮               "
+--" ┃┃╰╯┃┃╱╱╱╱╭╯╰╮              "      
+--" ┃╭╮╭╮┣━━┳━┻╮╭╋━━┳━╮           "
+--" ┃┃┃┃┃┃╭╮┃━━┫┃┃┃━┫╭┫           "
+--" ┃┃┃┃┃┃╭╮┣━━┃╰┫┃━┫┃             "
+--" ╰╯╰╯╰┻╯╰┻━━┻━┻━━┻╯             "
 
---[[ 
-▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀ 
-▀▄ ▄▀                                      ▀▄ ▄▀ 
-      by member oscar team @PXPP3
-    -----OSCAR TEAM
-      please join to Channel Oscar Team @oscarteam
-    Plugin ; lock audio with warn
-
-▀▄ ▄▀                                      ▀▄ ▄▀ 
-▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀ 
---]] 
 do 
 
 local function pre_process(msg) 
-local monster = msg['id']
+local master = msg['id']
 local  user = msg.from.id
 local chat = msg.to.id 
     local oscar = 'mate:'..msg.to.id 
@@ -25,38 +20,37 @@ local chat = msg.to.id
         return msg 
     end 
 
-local function run(msg, matches) 
-local monster = msg['id'] 
+local function masterteam(msg, matches) 
+local master = msg['id'] 
     chat_id =  msg.to.id 
-    if matches[1] == 'kik' and matches[2] == "audio" and is_momod(msg) then 
+    if matches[1] == 'kick' and matches[2] == "audio" and is_momod(msg) then 
                     local oscar = 'mate:'..msg.to.id 
                     redis:set(oscar, true) 
-                    local dd = 'تم قفل بصمات مع طرد{✔}😈'
-reply_msg(monster, dd, ok_cb, true) 
-elseif matches[1] == 'kik' and matches[2] == 'audio' and not is_momod(msg) then 
-local pxpp = 'للمشرفين فقط عزيزي👿' 
-reply_msg(monster, pxpp, ok_cb, true) 
+                    local text = 'تم قفل البصمات مع طرد{✔}😈'
+reply_msg(master, text, ok_cb, true) 
+elseif matches[1] == 'kick' and matches[2] == 'audio' and not is_momod(msg) then 
+local text = 'للمشرفين فقط عزيزي👿' 
+reply_msg(master, text, ok_cb, true) 
 
-    elseif matches[1] == 'unkik' and matches[2] == 'audio' and is_momod(msg) then 
+    elseif matches[1] == 'ukick' and matches[2] == 'audio' and is_momod(msg) then 
       local oscar = 'mate:'..msg.to.id 
       redis:del(oscar) 
-    local gg = ' تم الغاء قفل بصمات مع طرد{❎}😈' 
-reply_msg(monster, gg, ok_cb, true) 
-elseif matches[1] == 'unkik' and matches[2] == 'audio' and not is_momod(msg) then 
-local pxff = 'للمشرفين فقط عزيزي 👿'
-reply_msg(monster, pxff, ok_cb, true) 
+    local text = ' تم الغاء قفل بصمات مع طرد{❎}😈' 
+reply_msg(master, text, ok_cb, true) 
+elseif matches[1] == 'ukick' and matches[2] == 'audio' and not is_momod(msg) then 
+local text = 'للمشرفين فقط عزيزي 👿'
+reply_msg(master, text, ok_cb, true) 
 end 
 end 
 
 return { 
     patterns = { 
-        '^[!/#](kik) (.*)$', 
-       '^[!/#](unkik) (.*)$' 
+        '^[!/#](kick) (.*)$', 
+       '^[!/#](ukick) (.*)$' 
     }, 
-    run = run, 
+    run = masterteam, 
     pre_process = pre_process 
 } 
 
--- By @PXPP3
---Oscar Team
 end 
+--MASTER TEAM -_-
