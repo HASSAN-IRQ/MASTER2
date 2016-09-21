0c3634cd71cc1c2d423fb5bbf39fb21cff21e077
masterteam1 <zozo.prince7@gmail.com>
2016-09-21 16:45:59 +0300

    master_team

--- plugins/linkpv.lua
+++ plugins/linkpv.lua
@@ -1,24 +1,36 @@
+--" ╭━╮╭━╮╱╱╱╱╱╭╮               "
+--" ┃┃╰╯┃┃╱╱╱╱╭╯╰╮              "      
+--" ┃╭╮╭╮┣━━┳━┻╮╭╋━━┳━╮           "
+--" ┃┃┃┃┃┃╭╮┃━━┫┃┃┃━┫╭┫           "
+--" ┃┃┃┃┃┃╭╮┣━━┃╰┫┃━┫┃             "
+--" ╰╯╰╯╰┻╯╰┻━━┻━┻━━┻╯             "
+
 do
 
-function run(msg, matches)
+function masterteam(msg, matches)
+local reply_id = msg['id']
        if not is_momod(msg) then
-        return "only for sudo"
+        local master = "فقط للمشرفين عزيزي"
+        reply_msg(reply_id, master, ok_cb, true) 
        end
     local data = load_data(_config.moderation.data)
       local group_link = data[tostring(msg.to.id)]['settings']['set_link']
        if not group_link then 
-        return "first create newlink"
+        local master = "لا يوجد رابط للمجموعه.. قم بأنشاء رابط اولا."
+        reply_msg(reply_id, master, ok_cb, true) 
        end
-         local text = "Group link\n"..group_link
+         local text = "رابط المجموعه:\n"..group_link
           send_large_msg('user#id'..msg.from.id, text, ok_cb, false)
-           return "link sent to pv"
+           local master = "تم ارسال الرابط .."
+           reply_msg(reply_id, master, ok_cb, true) 
 end
 
 return {
   patterns = {
     "^[/#!]([Ll]inkpv)$"
   },
-  run = run
+  run = masterteam
 }
 
 end
+--MASTER TEAM -_-
