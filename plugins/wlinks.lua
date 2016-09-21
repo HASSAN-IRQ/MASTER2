c51903571213f7da5362c52ca3cc5ed6b5c47dfe
masterteam1 <zozo.prince7@gmail.com>
2016-09-21 21:18:56 +0300

    master_team

--- plugins/wlinks.lua
+++ plugins/wlinks.lua
@@ -1,18 +1,14 @@
---[[ 
-▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀ 
-▀▄ ▄▀                                      ▀▄ ▄▀ 
-      by member oscar team @XVIPER 
-    -----OSCAR TEAM 
-      please join to Channel Oscar Team @oscarteam 
-    Plugin ; lock link with warn 
+--" ╭━╮╭━╮╱╱╱╱╱╭╮               "
+--" ┃┃╰╯┃┃╱╱╱╱╭╯╰╮              "      
+--" ┃╭╮╭╮┣━━┳━┻╮╭╋━━┳━╮           "
+--" ┃┃┃┃┃┃╭╮┃━━┫┃┃┃━┫╭┫           "
+--" ┃┃┃┃┃┃╭╮┣━━┃╰┫┃━┫┃             "
+--" ╰╯╰╯╰┻╯╰┻━━┻━┻━━┻╯             "
 
-▀▄ ▄▀                                      ▀▄ ▄▀ 
-▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀ 
---]] 
 do 
 
 local function pre_process(msg) 
-local monster = msg['id'] 
+local master = msg['id'] 
 local rash = 'XVIPERO:'..msg.to.id 
 local link = msg.text:match("[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm]%.[Mm][Ee]/") or msg.text:match("[Tt][Ll][Gg][Rr][Mm]%.[Mm][Ee]/") or msg.text:match("[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm]%.[Oo][Rr][Gg]") or msg.text:match("[Gg][Oo][Oo]%.[Gg][Li]/") or msg.text:match("[Aa][Dd][Ff]%.[Ll][Yy]/") or msg.text:match("[Bb][Ii][Tt]%.[Ll][Yy]") or msg.text:match("[Cc][Ff]%.[Ll][Yy]/") or msg.text:match("[Bb][Vv]%.[Vv][Cc]/") or msg.text:match("[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm]%.[Mm][Ee]") 
 local bot = msg.text:match("?[Ss][Tt][Aa][Rr][Tt]=") or msg.text:match("?[Ss][Tt][Oo][Pp]=") 
@@ -20,45 +16,43 @@ local bot = msg.text:match("?[Ss][Tt][Aa][Rr][Tt]=") or msg.text:match("?[Ss][Tt
             delete_msg(msg.id, ok_cb, true) 
             delete_msg(msg.id, ok_cb, true) 
             delete_msg(msg.id, ok_cb, true) 
-            local dea = " 🔹عزيزي : "..msg.from.first_name.."".."\n".."🔹يمنع نشر (روابط - اعلانات - قنوات)🍃".."\n".."🔹التزم بالقوانين🚫 ".."\n".."🔹 يا : @"..(msg.from.username or " ") 
-reply_msg(monster, dea, ok_cb, true) 
+            local text = " 🔹عزيزي : "..msg.from.first_name.."".."\n".."🔹يمنع نشر (روابط - اعلانات - قنوات)🍃".."\n".."🔹التزم بالقوانين🚫 ".."\n".."🔹 يا : @"..(msg.from.username or " ") 
+reply_msg(master, text, ok_cb, true) 
         end
         return msg 
     end 
 
-local function monster(msg, matches) 
-local monster = msg['id'] 
+local function masterteam(msg, matches) 
+local master = msg['id'] 
     chat_id =  msg.to.id 
-    if matches[1] == 'warn' and matches[2] == "links" and is_momod(msg) then 
+    if matches[1] == 'wr' and matches[2] == "links" and is_momod(msg) then 
                     local rash = 'XVIPERO:'..msg.to.id 
                     redis:set(rash, true) 
-                    local dd = 'تم قفل الروبط مع تحدير{✔}😈' 
-reply_msg(monster, dd, ok_cb, true) 
-elseif matches[1] == 'warn' and matches[2] == 'links' and not is_momod(msg) then 
-local pxpp = 'للمشرفين فقط✋👿' 
-reply_msg(monster, pxpp, ok_cb, true) 
+                    local text = 'تم قفل الروابط مع تحذير{✔}😈'
+reply_msg(master, text, ok_cb, true) 
+elseif matches[1] == 'wr' and matches[2] == 'links' and not is_momod(msg) then 
+local text = 'للمشرفين فقط✋👿' 
+reply_msg(master, text, ok_cb, true) 
 
-    elseif matches[1] == 'uwarn' and matches[2] == 'links' and is_momod(msg) then 
+    elseif matches[1] == 'uwr' and matches[2] == 'links' and is_momod(msg) then 
       local rash = 'XVIPERO:'..msg.to.id 
       redis:del(rash) 
-    local gg = ' تم الغاء قفل الروابط مع تحذير {❎}😈' 
-reply_msg(monster, gg, ok_cb, true) 
-elseif matches[1] == 'uwarn' and matches[2] == 'links' and not is_momod(msg) then 
-local pxff = 'للمشرفين فقط✋👿' 
-reply_msg(monster, pxff, ok_cb, true) 
+    local text = ' تم الغاء قفل الروابط مع تحذير {❎}😈' 
+reply_msg(master, text, ok_cb, true) 
+elseif matches[1] == 'uwr' and matches[2] == 'links' and not is_momod(msg) then 
+local text = 'للمشرفين فقط✋👿' 
+reply_msg(master, text, ok_cb, true) 
 end 
 end 
 
 return { 
     patterns = { 
-        '^[!/#](warn) (.*)$', 
-        '^[!/#](uwarn) (.*)$' 
+        '^[!/#](wr) (.*)$', 
+        '^[!/#](uwr) (.*)$' 
     }, 
-    run = monster, 
+    run = masterteam, 
     pre_process = pre_process 
 } 
 
--- By @XVIPER 
---Oscar Team 
 end 
-    
+--MASTER TEAM -_-    
