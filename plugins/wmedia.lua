df7b8f986ec1b158d03f4b4e0184c3c40c8e2b8c
masterteam1 <zozo.prince7@gmail.com>
2016-09-21 21:24:38 +0300

    master_team

--- plugins/wmedia.lua
+++ plugins/wmedia.lua
@@ -1,23 +1,21 @@
---[[ 
-▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀ 
-▀▄ ▄▀                                      ▀▄ ▄▀ 
-▀▄ لتحذف حقوق بذمتك🚶🏻 
-      #CODS CREATED By ~ @JALAL_ALDON 
-      please join to Channel Oscar Team @OSCARBOTv2 
-▀▄ ▄▀                                      ▀▄ ▄▀ 
-▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀ 
---]] 
+--" ╭━╮╭━╮╱╱╱╱╱╭╮               "
+--" ┃┃╰╯┃┃╱╱╱╱╭╯╰╮              "      
+--" ┃╭╮╭╮┣━━┳━┻╮╭╋━━┳━╮           "
+--" ┃┃┃┃┃┃╭╮┃━━┫┃┃┃━┫╭┫           "
+--" ┃┃┃┃┃┃╭╮┣━━┃╰┫┃━┫┃             "
+--" ╰╯╰╯╰┻╯╰┻━━┻━┻━━┻╯             "
+
 do 
 
 local function pre_process(msg) 
-local jalal = msg['id'] 
+local master = msg['id'] 
   local user = msg.from.id 
 local chat = msg.to.id 
     local oscar = 'mate:'..msg.to.id 
     if redis:get(oscar) and msg.media and not is_momod(msg) then 
     delete_msg(msg.id, ok_cb, false) 
 local test = "عزيزي👿~["..msg.from.first_name.."]".."\n".."يمنع نشر صور فيديوهات صوتيات وكافة الميديا هنا ان تكرر الامر سوف تجبرني على طردك{❌}👿".."\n".."👾username: @"..(msg.from.username or " ") 
-reply_msg(jalal, test, ok_cb, true) 
+reply_msg(master, test, ok_cb, true) 
 
 end 
 
@@ -25,32 +23,32 @@ end
     end 
 
 local function run(msg, matches) 
-local jalal = msg['id'] 
+local master = msg['id'] 
 
-    if matches[1] == 'warn media'  and is_momod(msg) then 
+    if matches[1] == 'wr media'  and is_momod(msg) then 
                     local oscar = 'mate:'..msg.to.id 
                     redis:set(oscar, true) 
-                    local oscar1 = ' تم كتم جميع الوسائط مع تحدير🔕 ' 
-reply_msg(jalal, oscar1, ok_cb, true) 
-elseif matches[1] == 'warn media' and not is_momod(msg) then 
-local asdy = 'للمشرفين فقط🔴' 
-reply_msg(jalal, asdy, ok_cb, true) 
+                    local text = ' تم كتم جميع الوسائط مع تحدير🔕 ' 
+reply_msg(master, text, ok_cb, true) 
+elseif matches[1] == 'wr media' and not is_momod(msg) then 
+local text = 'للمشرفين فقط🔴' 
+reply_msg(master, text, ok_cb, true) 
 
-    elseif matches[1] == 'uwarn media'  and is_momod(msg) then 
+    elseif matches[1] == 'uwr media'  and is_momod(msg) then 
       local oscar = 'mate:'..msg.to.id 
       redis:del(oscar) 
-    local don = ' تم الغاء كتم الوسائط مع تحدير🔔' 
-reply_msg(jalal, don, ok_cb, true) 
-elseif matches[1] == 'uwarn media' and not is_momod(msg) then 
-local jalal_aldon = 'للمشرفين فقط🔴' 
-reply_msg(jalal, jalal_aldon, ok_cb, true) 
+    local text = ' تم الغاء كتم الوسائط مع تحدير🔔' 
+reply_msg(master, text, ok_cb, true) 
+elseif matches[1] == 'uwr media' and not is_momod(msg) then 
+local text = 'للمشرفين فقط🔴' 
+reply_msg(master, text, ok_cb, true) 
 end 
 end 
 
 return { 
     patterns = { 
-    "^[!/#](warn media)$", 
-    "^[!/#](uwarn media)$" 
+    "^[!/#](wr media)$", 
+    "^[!/#](uwr media)$" 
     }, 
 run = run, 
     pre_process = pre_process 
